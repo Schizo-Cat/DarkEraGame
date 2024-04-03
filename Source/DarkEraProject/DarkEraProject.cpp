ac9a21a20a633bc3307d2131d7279a5c52e4f23d
Bogdan Panow <GoggiProsto@gmail.com>
2024-04-03 15:44:09 +0200

    Project initialization

---
+++ Source/DarkEraProject/DarkEraProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DarkEraProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DarkEraProject, "DarkEraProject" );
