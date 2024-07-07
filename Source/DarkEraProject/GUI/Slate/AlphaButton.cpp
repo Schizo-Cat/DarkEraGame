2cde37ba018ca6dd9ee8b5afe9772e4c8349dc1b
Bogdan Panow <GoggiProsto@gmail.com>
2024-07-07 23:08:59 +0200

    Fight system v 1.0

--- Source/DarkEraProject/GUI/Slate/AlphaButton.cpp
+++ Source/DarkEraProject/GUI/Slate/AlphaButton.cpp
@@ -1,92 +1,207 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #include "AlphaButton.h"
-#include "Components/Image.h"
-#include "Engine/Texture2D.h"
 
-void UAlphaButton::NativeConstruct()
+#include "Components/ButtonSlot.h"
+#include "Components/CanvasPanel.h"
+
+#pragma region >>> SAlphaButton <<<
+
+void SAlphaButton::SetTextureData(UTexture2D* InTexture)
 {
-    Super::NativeConstruct();
-
-    if (ButtonImage)
-    {
-        // Use the getter method for the brush texture
-        ButtonTexture = Cast<UTexture2D>(ButtonImage->GetBrush().GetResourceObject());
-    }
+	TextureData = static_cast<FColor*>((InTexture->GetPlatformData()->Mips[0]).BulkData.Lock(LOCK_READ_ONLY));
+	ImageHeight = InTexture->GetPlatformData()->SizeY;
+	ImageWidth = InTexture->GetPlatformData()->SizeX;
+	InTexture->GetPlatformData()->Mips[0].BulkData.Unlock();
 }
 
-void UAlphaButton::NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+FReply SAlphaButton::OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
 {
-    Super::NativeOnMouseEnter(InGeometry, InMouseEvent);
-
-    FVector2D LocalMousePosition = InGeometry.AbsoluteToLocal(InMouseEvent.GetScreenSpacePosition());
-
-    if (IsOpaquePixel(LocalMousePosition.X, LocalMousePosition.Y))
-    {
-        OnMouseHovered();
-    }
-    else
-    {
-        OnMouseUnhovered();
-    }
+	if (TextureData)
+	{
+		if (CachedOverlap) 
+		{
+			return SButton::OnMouseButtonDown(MyGeometry, MouseEvent);
+		}
+		return FReply::Unhandled();
+	}
+	return SButton::OnMouseButtonDown(MyGeometry, MouseEvent);
 }
 
-void UAlphaButton::NativeOnMouseLeave(const FPointerEvent& InMouseEvent)
+FReply SAlphaButton::OnMouseButtonDoubleClick(const FGeometry& InMyGeometry, const FPointerEvent& InMouseEvent) 
 {
-    Super::NativeOnMouseLeave(InMouseEvent);
-    OnMouseUnhovered();
+	if (TextureData)
+	{
+		if (CachedOverlap)
+		{
+			return SButton::OnMouseButtonDoubleClick(InMyGeometry, InMouseEvent);
+		}
+		return FReply::Unhandled();
+	}
+	return SButton::OnMouseButtonDoubleClick(InMyGeometry, InMouseEvent);
 }
 
-FReply UAlphaButton::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+FReply SAlphaButton::OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
 {
-    FVector2D LocalMousePosition = InGeometry.AbsoluteToLocal(InMouseEvent.GetScreenSpacePosition());
-    if (ButtonTexture && IsOpaquePixel(LocalMousePosition.X, LocalMousePosition.Y))
-    {
-        OnButtonClicked();
-        return FReply::Handled();
-    }
-    return FReply::Unhandled();
+	if (TextureData)
+	{
+		if (CachedOverlap && IsPressed())
+		{
+			return SButton::OnMouseButtonUp(MyGeometry, MouseEvent);
+		}
+		return FReply::Unhandled();
+	}
+	return SButton::OnMouseButtonUp(MyGeometry, MouseEvent);
 }
 
-bool UAlphaButton::IsOpaquePixel(int32 X, int32 Y)
+void SAlphaButton::OnMouseEnter(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
 {
-    if (!ButtonTexture) return false;
-
-    FTexturePlatformData* PlatformData = ButtonTexture->GetPlatformData();
-    if (!PlatformData) return false;
-
-    const FColor* FormData = static_cast<const FColor*>(PlatformData->Mips[0].BulkData.Lock(LOCK_READ_ONLY));
-    if (!FormData) return false;
-
-    FColor PixelColor = FormData[Y * PlatformData->SizeX + X];
-    PlatformData->Mips[0].BulkData.Unlock();
-
-    return PixelColor.A > 0;
+	if (TextureData)
+	{
+		if (CachedOverlap)
+		{
+			return SButton::OnMouseEnter(MyGeometry, MouseEvent);
+		}
+		else
+		{
+			if(IsMouseOverOpaquePixel( MouseEvent))
+			{
+				CachedOverlap = true;
+				return SButton::OnMouseEnter(MyGeometry, MouseEvent);
+			}
+			else
+			{
+				CachedOverlap = false;
+				return;
+			}
+		}
+	}
+	return SButton::OnMouseEnter(MyGeometry, MouseEvent);
 }
 
-void UAlphaButton::OnMouseHovered()
+void SAlphaButton::OnMouseLeave(const FPointerEvent& MouseEvent) 
 {
-    // Handle the hover effect here
-    // For example, change the button color or show an outline
-    if (ButtonImage)
-    {
-        // Example hover effect
-        ButtonImage->SetOpacity(0.8f);
-    }
+	if(TextureData)
+	{
+		if (CachedOverlap)
+		{
+			CachedOverlap = false;
+			return SButton::OnMouseLeave(MouseEvent);
+		}
+	}
+	return SButton::OnMouseLeave(MouseEvent);
 }
 
-void UAlphaButton::OnMouseUnhovered()
+bool SAlphaButton::IsMouseOverOpaquePixel( const FPointerEvent & MouseEvent) const
 {
-    // Handle the unhover effect here
-    // For example, revert the button color or hide the outline
-    if (ButtonImage)
-    {
-        // Example unhover effect
-        ButtonImage->SetOpacity(1.0f);
-    }
+	bool OverOpaque = true;
+	
+	const FGeometry MyGeometry = GetCachedGeometry();
+	FVector2D LocalPosition = MyGeometry.AbsoluteToLocal(MouseEvent.GetScreenSpacePosition());
+	LocalPosition.X = floor(LocalPosition.X);
+	LocalPosition.Y = floor(LocalPosition.Y);
+	LocalPosition /= MyGeometry.GetLocalSize();
+	
+	if (LocalPosition.X > 1.0f || LocalPosition.Y > 1.0f || LocalPosition.X<0.0f || LocalPosition.Y<0.0f)
+	{
+		return false;
+	}
+	
+	
+	if (!TextureData) 
+	{
+		OverOpaque = false;
+	}
+	else 
+	{
+		LocalPosition.X *= ImageWidth;
+		LocalPosition.Y *= ImageHeight;
+		const int BufferPosition = ((int)LocalPosition.Y * ImageWidth) + (int)LocalPosition.X;
+		if (TextureData[BufferPosition].A <= AdvancedHitAlpha) 
+			OverOpaque = false; 
+	}
+	
+	return OverOpaque;
 }
 
-void UAlphaButton::OnButtonClicked()
+FReply SAlphaButton::OnMouseMove(const FGeometry & MyGeometry, const FPointerEvent & MouseEvent)
 {
-    // Handle the button click event
-    // This can be customized as needed
-    UE_LOG(LogTemp, Warning, TEXT("Button clicked!"));
+	if (!TextureData) 
+		return SButton::OnMouseMove(MyGeometry, MouseEvent);
+
+	if(IsMouseOverOpaquePixel(MouseEvent))
+	{
+		if (!CachedOverlap)
+		{
+			CachedOverlap = true;
+			OnMouseEnter(MyGeometry, MouseEvent);
+		}
+	}
+	else
+	{
+		if (CachedOverlap)
+		{
+			CachedOverlap = false;
+			OnMouseLeave(MouseEvent);
+		}
+	}
+	
+	return FReply::Handled();
 }
+
+
+FCursorReply SAlphaButton::OnCursorQuery(const FGeometry & MyGeometry, const FPointerEvent & CursorEvent) const
+{
+	if (!IsHovered()) return FCursorReply::Unhandled();
+	TOptional<EMouseCursor::Type> TheCursor = GetCursor();
+	return (TheCursor.IsSet()) ? FCursorReply::Cursor(TheCursor.GetValue()) : FCursorReply::Unhandled();
+}
+
+#pragma endregion
+
+#pragma region >>> UAlphaButton <<<
+
+UAlphaButton::UAlphaButton(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer),
+	  AdvancedHitTexture(nullptr),
+	  AdvancedHitAlpha(0)
+{
+}
+
+void UAlphaButton::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+	
+	if(MyButton.IsValid())
+	{
+		if(AdvancedHitTexture) (static_cast<SAlphaButton*>(MyButton.Get()))->SetTextureData(AdvancedHitTexture);
+		(static_cast<SAlphaButton*>(MyButton.Get()))->SetAdvancedHitAlpha(AdvancedHitAlpha);
+	}
+}
+
+
+TSharedRef<SWidget> UAlphaButton::RebuildWidget()
+{
+	const TSharedPtr<SAlphaButton> AlphaButton = SNew(SAlphaButton)
+		.OnClicked(BIND_UOBJECT_DELEGATE(FOnClicked, SlateHandleClicked))
+		.OnPressed(BIND_UOBJECT_DELEGATE(FSimpleDelegate, SlateHandlePressed))
+		.OnReleased(BIND_UOBJECT_DELEGATE(FSimpleDelegate, SlateHandleReleased))
+		.OnHovered_UObject(this, &ThisClass::SlateHandleHovered)
+		.OnUnhovered_UObject(this, &ThisClass::SlateHandleUnhovered)
+		.ButtonStyle(&GetStyle())
+		.ClickMethod(GetClickMethod())
+		.TouchMethod(GetTouchMethod())
+		.IsFocusable(GetIsFocusable())
+		;
+
+	if(AdvancedHitTexture) AlphaButton->SetTextureData(AdvancedHitTexture);
+	
+	AlphaButton->SetAdvancedHitAlpha(AdvancedHitAlpha);
+
+	MyButton = AlphaButton;
+
+	if (GetChildrenCount() > 0) Cast<UButtonSlot>(GetContentSlot())->BuildSlot(MyButton.ToSharedRef());
+
+	return MyButton.ToSharedRef();
+}
+#pragma endregion
--- Source/DarkEraProject/GUI/Slate/AlphaButton.h
+++ Source/DarkEraProject/GUI/Slate/AlphaButton.h
@@ -1,35 +1,83 @@
-#include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Components/Button.h"
 #include "AlphaButton.generated.h"
 
-class UTexture2D;
-class UImage;
+/**
+ * 
+ */
+
+class DARKERAPROJECT_API SAlphaButton : public SButton {
+	
+public:
+
+	FColor* TextureData;
+
+	int32 ImageWidth;
+
+	int32 ImageHeight;
+	
+	int AdvancedHitAlpha;
+
+	bool CachedOverlap;
+
+	SAlphaButton() : TextureData(nullptr), ImageWidth(0), ImageHeight(0), AdvancedHitAlpha(0),
+	                 CachedOverlap(false)
+	{
+	}
+
+	void SetTextureData(UTexture2D* InTexture);
+	
+	void SetAdvancedHitAlpha(const int InAlpha) 
+	{ AdvancedHitAlpha = InAlpha; }
+
+	virtual FReply OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseButtonDoubleClick(const FGeometry& InMyGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual FReply OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual FReply OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual void OnMouseEnter(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent) override;
+	virtual void OnMouseLeave(const FPointerEvent& MouseEvent) override;
+	virtual FCursorReply OnCursorQuery(const FGeometry& MyGeometry, const FPointerEvent& CursorEvent) const override;
+	virtual TSharedPtr<IToolTip> GetToolTip() override
+	{ 
+		return (IsHovered() ? SWidget::GetToolTip() : nullptr);
+	}
+
+	bool IsMouseOverOpaquePixel( const FPointerEvent & MouseEvent) const;
+
+	
+};
 
 UCLASS()
-class DARKERAPROJECT_API UAlphaButton : public UUserWidget
+class DARKERAPROJECT_API UAlphaButton : public UButton
 {
-    GENERATED_BODY()
+	GENERATED_UCLASS_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AdvancedHitTest") 
+	UTexture2D* AdvancedHitTexture;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AdvancedHitTest", meta = (ClampMin = "0.0", ClampMax = "255.0", UIMin = "0.0", UIMax = "255.0")) 
+	int AdvancedHitAlpha;
 
-public:
-    virtual void NativeConstruct() override;
-    virtual void NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
-    virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
-    virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	UFUNCTION(BlueprintCallable, Category = "AdvancedHitTest") void SetAdvancedHitTexture(UTexture2D* InTexture) 
+	{
+		AdvancedHitTexture = InTexture;
+		if (MyButton.IsValid()) 
+		(static_cast<SAlphaButton*>(MyButton.Get()))->SetTextureData(AdvancedHitTexture);
+	}
+	UFUNCTION(BlueprintCallable, Category = "AdvancedHitTest") void SetAdvancedHitAlpha(const int InAlpha)
+	{
+		AdvancedHitAlpha = InAlpha;
+		if (MyButton.IsValid()) 
+			(static_cast<SAlphaButton*>(MyButton.Get()))->SetAdvancedHitAlpha(AdvancedHitAlpha);
+	}
 
-protected:
-    UPROPERTY(meta = (BindWidget))
-    UImage* ButtonImage;
+	TSharedPtr<SButton> GetCacheSButton()
+	{
+		return MyButton;
+	}
 
-    UTexture2D* ButtonTexture;
-
-    bool IsOpaquePixel(int32 X, int32 Y);
-
-    UFUNCTION()
-    void OnMouseHovered();
-
-    UFUNCTION()
-    void OnMouseUnhovered();
-
-    UFUNCTION()
-    void OnButtonClicked();
+	virtual void SynchronizeProperties() override;
+	virtual TSharedRef<SWidget> RebuildWidget() override;
 };
